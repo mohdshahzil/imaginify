1870f3203083537c4e4207753112a5dafdc7b27d
mohdshahzil <theshahzil@gmail.com>
2024-02-21 21:36:06 +0530

    form-added

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Header from "@/components/shared/Header";
 import { transformationTypes } from "@/constants";
+import TransformationForm from "@/components/shared/TransformationForm";
 
 const AddTransformationTypePage = ({ params: { type } }: SearchParamProps) => {
   const transformation = transformationTypes[type];
 
   return (
-    <Header
-      title={transformation.title}
-      subtitle={transformation.subTitle}
-    ></Header>
+    <>
+      <Header
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      ></Header>
+      <TransformationForm></TransformationForm>
+    </>
   );
 };
 
